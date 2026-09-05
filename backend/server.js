3a2803bec3f580401cc6f2ca581f4f5c4329136a
Prathibha-n28 <prathibhan523@gmail.com>
2026-09-05 15:31:10 +0530

    Tighten CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 
 const PORT = process.env.PORT||5000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://prathibha-n28.github.io"
+}));
 function authenticateToken(req, res, next) {
 
     const authHeader = req.headers["authorization"];
